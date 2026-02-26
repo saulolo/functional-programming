2b8ac6720471691c77b44f0663a60ebf7c1a65f3
saulolo <saulolo@gmail.com>
2026-02-26 16:18:46 -0500

    🎉 Initial commit

---
+++ src/test/java/edu/functional_programming/FunctionalProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.functional_programming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
